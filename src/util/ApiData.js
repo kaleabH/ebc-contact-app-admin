cdc7f4e9974068dbdc84dd09ffa4cc58fb8c9134
Kaleab Hailu <kaleabhailu46@gmail.com>
2023-02-17 10:34:24 +0300

    gitignored ApiData.js and contactsApi.js

--- src/util/ApiData.js
+++ src/util/ApiData.js
@@ -1,2 +1,8 @@
+export const url =
+  "https://api.jsonstorage.net/v1/json/b8b28667-4983-42a1-b60b-4aad38f39f99/07ddc6f8-5d5b-4e0b-91d0-9056035ed2f3";
+export const ApiKey = "0fd11bd1-3ecd-42c3-a3f9-a0a2b73f2b11";
+/*for the jsonbin api*/
+// baseURL: "https://api.jsonbin.io/v3/b/6389d05b003d6444ce60c326",
+
 /* for the jsonbin api*/
 // const key = "$2b$10$oyACctGkJDCjwuEKOcpRPO4dwjCqEFoYOznrF5ngbtxVzGLyyur2S";
--- src/util/contactsApi2.js
+++ src/util/contactsApi2.js
@@ -1,15 +1,10 @@
+import { url, ApiKey } from "./ApiData";
 const axios = require("axios");
 const api = axios.create({
-  /*for the jsonbin api*/
-  // baseURL: "https://api.jsonbin.io/v3/b/6389d05b003d6444ce60c326",
-
-  baseURL:
-    "https://api.jsonstorage.net/v1/json/b8b28667-4983-42a1-b60b-4aad38f39f99/07ddc6f8-5d5b-4e0b-91d0-9056035ed2f3",
+  baseURL: url,
 });
-/* for the jsonbin api*/
-// const key = "$2b$10$oyACctGkJDCjwuEKOcpRPO4dwjCqEFoYOznrF5ngbtxVzGLyyur2S";
 
-const key = "0fd11bd1-3ecd-42c3-a3f9-a0a2b73f2b11";
+const key = ApiKey;
 const config = {
   /*for the jsonbin api*/
   // headers: { "X-Master-Key": key },
